4305542dfd7f8b59812fcad18962139579c2a455
SalmanAd01 <salmanadhikari415@gmail.com>
2023-02-20 23:55:33 +0530

    added angular

--- angular_prac/test.js
+++ angular_prac/test.js
@@ -1,4 +1,9 @@
 var angular = angular.module("myApp", []);
 angular.controller("myController", function ($scope) {
   $scope.name = "World";
+
+  // define a function
+  $scope.sayHello = function () {
+    return "Hello " + $scope.name;
+  };
 });
