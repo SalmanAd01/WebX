e1ffed867dc67f1b470044b5ab61023d2a9cefd2
SalmanAd01 <salmanadhikari415@gmail.com>
2023-03-10 00:01:35 +0530

    autogenarted images

--- exp5/webpack-typescript-quickstart/src/index.ts
+++ exp5/webpack-typescript-quickstart/src/index.ts
@@ -1,15 +1,14 @@
 console.log("Hi")
 
 const IMAGE_LINKS = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRahGWeaTE3yK301ENIQCSA4rze5t6x3x1m25wiL18X4cgnOGvHTvHI6bcnMhjyfdCmPkI&usqp=CAU",
     "https://www.wired.com/images_blogs/threatlevel/2011/07/aaron_swartz.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9fnE1hmEZ39oCKKCdkdCxOQWBLcNRkGQNv7fQgFb1JG1y_odMly9-lZFUUv_CEPITrMQ&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRahGWeaTE3yK301ENIQCSA4rze5t6x3x1m25wiL18X4cgnOGvHTvHI6bcnMhjyfdCmPkI&usqp=CAU"
 ]
 
 const button = document.getElementById("im-change") as HTMLButtonElement
-
+var CURRENT_IMAGE = 0
 button.addEventListener("click", () => {
     const image = document.getElementById("im-image") as HTMLImageElement
-    const randomIndex = Math.floor(Math.random() * IMAGE_LINKS.length)
-    image.src = IMAGE_LINKS[randomIndex]
+    image.src = IMAGE_LINKS[++CURRENT_IMAGE % IMAGE_LINKS.length]
 })
